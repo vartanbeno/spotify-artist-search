d2f27e4e9b1ac0f94c6e2af9ad8b11dda47084ab
Vartan Benohanian <vartanbeno@gmail.com>
2019-02-26 17:27:02 -0500

    #3 Add Login component, have to handle logged in state
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import './App.scss';
 import Navbar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
 import { green, lightGreen, red } from '@material-ui/core/colors';
+import Login from './components/login/Login';
+import withStyles from '@material-ui/core/styles/withStyles';
+import queryString from 'querystring';
 
 const theme = createMuiTheme({
     palette: {
@@ -16,14 +18,32 @@ const theme = createMuiTheme({
     }
 });
 
+const style = {
+    root: {
+        display: 'flex',
+        flexFlow: 'column',
+    }
+};
+
 class App extends Component {
+    state = {
+        isLoggedIn: false
+    };
+
+    componentDidMount() {
+        const params = queryString.parse(window.location.hash.slice(1));            // slice(1) to ignore leading #
+    if (params.access_token && typeof params.access_token === 'string') {
+        window.localStorage.setItem('access-token', params.access_token);
+    }
+    }
+
     render() {
         return (
             <MuiThemeProvider theme={theme}>
                 <Router>
-                    <div className="App">
+                    <div className={`App ${this.props.classes.root}`}>
                         <Navbar/>
-                        <Route exact path="/"/>
+                        <Route exact path="/" component={this.state.isLoggedIn ? void 0 : Login}/>
                     </div>
                 </Router>
             </MuiThemeProvider>
@@ -31,4 +51,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default withStyles(style)(App);
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import './Navbar.scss';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
 
 class Navbar extends Component {
+    state = {
+        isLoggedIn: this.isLoggedIn()
+    };
+
+    isLoggedIn() {
+        return !!localStorage.getItem('access-token');
+    }
+
+    logout() {
+        window.localStorage.clear();
+    }
+
     render() {
         return (
             <header className="nav-container">
-                <AppBar position="static">
-                    <Toolbar className="title">
-                        <Typography variant="h5" color="inherit">
-                            Spotify Artist Search
-                        </Typography>
+                <AppBar position="static" color="inherit">
+                    <Toolbar className="navbar">
+                        <div className="title">
+                            <img src="assets/images/spotify-logo/spotify-logo-green.png" alt="Spotify Icon" className="spotify-logo"/>
+                            <Typography variant="h5" color="inherit">
+                                Artist Search
+                            </Typography>
+                        </div>
+                        <div className="buttons">
+                            {this.state.isLoggedIn ? <Button onClick={this.logout} variant="flat">Logout</Button> : null}
+                        </div>
                     </Toolbar>
                 </AppBar>
             </header>
