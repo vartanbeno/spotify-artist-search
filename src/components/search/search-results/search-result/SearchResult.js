30b222bb99fa8d63bf863f4165d2ed508f50dccd
Vartan Benohanian <vartanbeno@gmail.com>
2019-02-27 17:29:37 -0500

    #10 Create Loader component
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- src/components/search/search-results/search-result/SearchResult.js
+++ src/components/search/search-results/search-result/SearchResult.js
@@ -6,7 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
-import { CardActions } from '@material-ui/core';
+import { CardActionArea, CardActions } from '@material-ui/core';
 import StarIcon from '@material-ui/icons/Star';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import PeopleIcon from '@material-ui/icons/People';
@@ -30,11 +30,13 @@ class SearchResult extends Component {
         const stars = Math.ceil(popularity/20) || 1;
         return (
             <Card className={this.props.classes.card} elevation={3}>
-                <CardMedia className={this.props.classes.media} image={image ? image : 'assets/images/artist-default.png'} title={name}/>
-                <CardContent>
-                    <Typography variant="h6" noWrap>{name}</Typography>
-                    <Typography variant="body2" style={{ display: 'flex', alignItems: 'center' }}><PeopleIcon/>&nbsp;Followers: {numberOfFollowers.toLocaleString()}</Typography>
-                </CardContent>
+                <CardActionArea>
+                    <CardMedia className={this.props.classes.media} image={image ? image : 'assets/images/artist-default.png'} title={name}/>
+                    <CardContent>
+                        <Typography variant="h6" noWrap>{name}</Typography>
+                        <Typography variant="body2" style={{ display: 'flex', alignItems: 'center' }}><PeopleIcon/>&nbsp;Followers: {numberOfFollowers.toLocaleString()}</Typography>
+                    </CardContent>
+                </CardActionArea>
                 <CardActions style={{ padding: 16 }}>
                     {[...Array(stars)].map((e, i) => <StarIcon key={i} color="primary" style={{ margin: 0 }}/>)}
                     {[...Array(5 - stars)].map((e, i) => <StarBorderIcon key={i} color="primary" style={{ margin: 0 }}/>)}
