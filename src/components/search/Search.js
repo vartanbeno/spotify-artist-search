b10496158ae398804f5d24955253203e7f80724a
Vartan Benohanian <vartanbeno@gmail.com>
2019-03-01 18:11:50 -0500

    Suggestions as user types a query (#26)
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
-import { FormControlLabel, IconButton, InputAdornment, Radio, RadioGroup } from '@material-ui/core';
+import {
+    Avatar,
+    FormControlLabel,
+    IconButton,
+    InputAdornment,
+    List,
+    ListItem, ListItemText,
+    Radio,
+    RadioGroup
+} from '@material-ui/core';
 import './Search.scss';
 import TextField from '@material-ui/core/TextField';
 import SearchOutlined from '@material-ui/icons/SearchOutlined';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import SpotifyService from '../../services/SpotifyService';
+import ArtistSuggestion from '../../models/artist/ArtistSuggestion';
 
 class Search extends Component {
 
     state = {
         q: '',
-        limit: '10'
+        limit: '10',
+        searchAsYouType: null,
+        showSearchSuggestionStates: {
+            searchBarFocused: false,
+            listFocused: false
+        },
+        searchSuggestions: []
     };
 
     setQuery = e => {
         this.setState({
             [e.target.name]: e.target.value
-        });
+        }, () => this.searchAsYouType(this.state.q));
     };
 
     setLimit = e => {
@@ -25,11 +42,82 @@ class Search extends Component {
     };
 
     submitSearch = e => {
+        clearTimeout(this.state.searchAsYouType);
         e.preventDefault();
         this.props.history.push(`/search?q=${this.state.q}&limit=${this.state.limit}`);
         if (this.props.searchArtists) {
             this.props.searchArtists();
         }
+    };
+
+    searchAsYouType = q => {
+        clearTimeout(this.state.searchAsYouType);
+
+        if (!q) {
+            this.setState({ searchSuggestions: [] });
+            return;
+        }
+
+        this.setState({
+            searchAsYouType: setTimeout(() => {
+                SpotifyService.searchArtists(q, 5).then(
+                    res => {
+
+                        const artists = [];
+                        res.data.artists.items.forEach(artist => {
+
+                            const { id, name, images } = artist;
+                            const image = images.length ? images[0].url : null;
+
+                            artists.push(new ArtistSuggestion(id, name, image));
+
+                        });
+
+                        this.setState({
+                            searchSuggestions: artists
+                        });
+
+                    },
+                    err => console.log(err)
+                );
+            }, 350)
+        })
+    };
+
+    focusSearchBar = e => {
+        this.setState({
+            showSearchSuggestionStates: {
+                ...this.state.showSearchSuggestionStates,
+                searchBarFocused: true
+            }
+        });
+    };
+
+    unfocusSearchBar = e => {
+        this.setState({
+            showSearchSuggestionStates: {
+                ...this.state.showSearchSuggestionStates,
+                searchBarFocused: false
+            }
+        });
+    };
+
+    focusList = e => {
+        this.setState({
+            showSearchSuggestionStates: {
+                ...this.state.showSearchSuggestionStates,
+                listFocused: true
+            }
+        });
+    };
+
+    unfocusList = e => {
+        this.setState({
+            showSearchSuggestionStates: {
+                ...this.state.showSearchSuggestionStates,
+                listFocused: false
+            }
+        });
     };
 
     render() {
@@ -51,6 +139,8 @@ class Search extends Component {
                         autoComplete="off"
                         autoFocus={true}
                         onChange={this.setQuery}
+                        onFocus={this.focusSearchBar}
+                        onBlur={this.unfocusSearchBar}
                         InputProps={{
                             endAdornment: (
                                 <InputAdornment position="end">
@@ -61,6 +151,23 @@ class Search extends Component {
                             )
                         }}
                     />
+                    {
+                        this.state.searchSuggestions.length && (this.state.showSearchSuggestionStates.searchBarFocused || this.state.showSearchSuggestionStates.listFocused) ?
+                            <div className="search-suggestions-container" onMouseEnter={this.focusList} onBlur={this.unfocusList}>
+                                <List className="suggestion-list">
+                                    {this.state.searchSuggestions.map(artist =>
+                                        <Link key={artist.id} to={`/artist/${artist.id}/albums`} className="artist-link">
+                                            <ListItem button>
+                                                <Avatar src={artist.image ? artist.image : "/assets/images/artist-default.png"}/>
+                                                <ListItemText color="primary" primary={artist.name}/>
+                                            </ListItem>
+                                        </Link>
+                                    )}
+                                </List>
+                            </div>
+                            :
+                            null
+                    }
                 </form>
             </div>
         );
