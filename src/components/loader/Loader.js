0dd2b3013865b69c1b570911631098af64a77cc0
Vartan Benohanian <vartanbeno@gmail.com>
2019-03-02 11:24:58 -0500

    Make the Loader a functional component
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Loader.scss';
 import { CircularProgress } from '@material-ui/core';
 
-class Loader extends Component {
-
-    render() {
-        return (
-            <div className="loader-container">
-                <CircularProgress/>
-            </div>
-        )
-    }
-}
+const Loader = () => {
+    return (
+        <div className="loader-container">
+            <CircularProgress/>
+        </div>
+    )
+};
 
 export default Loader;
